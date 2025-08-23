4a72b5fe771ce47665dbda9b88f95271ad1cb8fa
LuKe <demon80706@gmail.com>
2025-08-23 12:29:23 +0630

    Update User.js

--- models/User.js
+++ models/User.js
@@ -5,9 +5,10 @@ const UserSchema = new mongoose.Schema({
   firstName: String,
   lastName: String,
   username: String,
-  coins: { type: Number, default: 100 },  // Start with 100 coins
+  coins: { type: Number, default: 0 },
   spins: { type: Number, default: 0 },
   referrals: { type: Number, default: 0 },
+  dailyTasks: { type: Object, default: {} } // { "2025-08-23": 3 }
 }, { timestamps: true });
 
 module.exports = mongoose.model("User", UserSchema);
