c22b3aa47e92314394ae38b727c799b13f942249
LuKe <demon80706@gmail.com>
2025-08-23 19:37:47 +0630

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -9,29 +9,9 @@ router.post("/login", async (req, res) => {
 
     let user = await User.findOne({ telegramId });
     if (!user) {
-      user = new User({
-        telegramId,
-        firstName,
-        lastName,
-        username,
-        photoUrl,
-        coins: 100
-      });
+      user = new User({ telegramId, firstName, lastName, username, photoUrl });
       await user.save();
     }
-
-    res.json({ success: true, user });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ success: false, message: "Server Error" });
-  }
-});
-
-// Get user profile
-router.get("/:telegramId", async (req, res) => {
-  try {
-    const user = await User.findOne({ telegramId: req.params.telegramId });
-    if (!user) return res.status(404).json({ success: false, message: "User not found" });
 
     res.json({ success: true, user });
   } catch (err) {
