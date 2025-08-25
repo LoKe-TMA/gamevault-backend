11e7708318f1c6bc7c14f9b824400a7769951e1e
LuKe <demon80706@gmail.com>
2025-08-25 17:54:03 +0630

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  telegramId: { type: String, unique: true },
-  firstName: String,
-  lastName: String,
-  username: String,
-  photoUrl: String,
-  coins: { type: Number, default: 0 },
-  referredBy: { type: String, default: null },
-  referrals: { type: Number, default: 0 },
-  completedTasks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Task" }]
-}, { timestamps: true });
+  telegramId: { type: String, required: true, unique: true },
+  username: { type: String },
+  first_name: { type: String },
+  last_name: { type: String },
+  photo_url: { type: String },
+  coin_balance: { type: Number, default: 0 },
+  referred_by: { type: String, default: null },
+  created_at: { type: Date, default: Date.now },
+});
 
 module.exports = mongoose.model("User", userSchema);
+chema);
