95bd4dd8ccaac601d2bf3234d14f82f07b380206
LuKe <demon80706@gmail.com>
2025-08-25 20:18:49 +0630

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
-const orderSchema = new mongoose.Schema({
+const OrderSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  game: { type: String, enum: ["PUBG", "MLBB"], required: true },
-  package: String,
+  game: String,
+  orderType: String,
   accountId: String,
   serverId: String,
-  coinsUsed: Number,
-  status: { type: String, enum: ["Pending", "Confirmed", "Rejected"], default: "Pending" }
+  cost: Number,
+  status: { type: String, default: "pending" }
 }, { timestamps: true });
 
-module.exports = mongoose.model("Order", orderSchema);
+module.exports = mongoose.model("Order", OrderSchema);
