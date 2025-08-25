7939d22475b4005249bdfb4a15806164fcc81193
LuKe <demon80706@gmail.com>
2025-08-25 17:54:38 +0630

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,22 +1,51 @@
 const express = require("express");
 const router = express.Router();
+const crypto = require("crypto");
 const User = require("../models/User");
 
-// Auto Register/Login
+// Telegram login verification
 router.post("/login", async (req, res) => {
-  try {
-    const { telegramId, firstName, lastName, username, photoUrl } = req.body;
+    const { initData, referred_by } = req.body;
 
-    let user = await User.findOne({ telegramId });
-    if (!user) {
-      user = new User({ telegramId, firstName, lastName, username, photoUrl, coins: 100 });
-      await user.save();
+    function verifyTelegramData(data) {
+        const telegramBotToken = process.env.TELEGRAM_BOT_TOKEN;
+        const secretKey = crypto.createHash('sha256').update(telegramBotToken).digest();
+        
+        const dataCheckString = Object.keys(data)
+            .filter(key => key !== 'hash')
+            .sort()
+            .map(key => `${key}=${data[key]}`)
+            .join('\n');
+
+        const hash = crypto.createHmac('sha256', secretKey)
+            .update(dataCheckString)
+            .digest('hex');
+
+        return hash === data.hash;
     }
 
-    res.json({ success: true, user });
-  } catch (err) {
-    res.status(500).json({ success: false, message: "Server Error" });
-  }
+    if (!verifyTelegramData(initData)) {
+        return res.status(400).json({ message: "Invalid Telegram Data" });
+    }
+
+    try {
+        let user = await User.findOne({ telegramId: initData.id });
+        if (!user) {
+            user = new User({
+                telegramId: initData.id,
+                username: initData.username,
+                first_name: initData.first_name,
+                last_name: initData.last_name,
+                photo_url: initData.photo_url,
+                referred_by: referred_by || null,
+            });
+            await user.save();
+        }
+        res.json({ success: true, user });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "Server error" });
+    }
 });
 
 module.exports = router;
