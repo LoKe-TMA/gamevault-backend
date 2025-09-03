2166a841871af14e2337fa9438276d2d3f90fc86
LuKe <demon80706@gmail.com>
2025-09-03 12:56:51 +0630

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,13 +1,43 @@
-// backend/models/User.js (update for referral)
-import mongoose from "mongoose";
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  telegramId: { type: String, unique: true },
-  name: String,
-  coins: { type: Number, default: 0 },
-  referrals: [{ type: String }],
-  referredBy: { type: String, default: null },
-  orders: [{ type: mongoose.Schema.Types.ObjectId, ref: "Order" }]
-}, { timestamps: true });
+    telegramId: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    firstName: {
+        type: String,
+        default: ''
+    },
+    username: {
+        type: String,
+        default: ''
+    },
+    coinBalance: {
+        type: Number,
+        default: 0
+    },
+    adWatchCount: {
+        type: Number,
+        default: 0
+    },
+    lastAdReset: {
+        type: Date,
+        default: Date.now
+    },
+    referralBonusReceived: {
+        type: Boolean,
+        default: false
+    },
+    referredBy: {
+        type: Number,
+        default: null
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
 
-export default mongoose.model("User", userSchema);
+module.exports = mongoose.model('User', userSchema);
